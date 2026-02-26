199779078904de6b6b0f2cdb9c6a1b8f5402551a
Zeeshan Ahmed Syed <Zeeshan.Syed@liwastores.com>
2026-02-26 09:40:13 +0400

    api keys added

--- api/sendPush.js
+++ api/sendPush.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const order = req.body;
 
   const message = {
-    app_id: "YOUR_ONESIGNAL_APP_ID",
+    app_id: "3854b8dd-3b56-434a-9e65-0a67564cf920",
     include_player_ids: ["PLAYER_ID_1"],
     headings: { "en": "New Order Received!" },
     contents: { "en": `Order #${order.id} placed by ${order.email}` }
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: "Basic YOUR_REST_API_KEY"
+      Authorization: "Basic ofyb7firtubtnb5jiu2aosyez"
     },
     body: JSON.stringify(message)
   });

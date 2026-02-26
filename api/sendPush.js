0af3f59ee0e6ad18bfa496d89cfcc53f33bfd6d1
Zeeshan Ahmed Syed <Zeeshan.Syed@liwastores.com>
2026-02-26 13:55:21 +0400

    api keys new app created in onesignal v66

--- api/sendPush.js
+++ api/sendPush.js
@@ -7,20 +7,23 @@ export default async function handler(req, res) {
     const order = req.body;
 
     const message = {
-      app_id: "3854b8dd-3b56-434a-9e65-0a67564cf920", 
+      app_id: "3854b8dd-3b56-434a-9e65-0a67564cf920",
       include_player_ids: ["95365288-8e21-4261-997e-0a13d87e1c89"],
       headings: { en: "New Order Received!" },
       contents: { en: `Order #${order.id} from ${order.email}` },
     };
 
-    const response = await fetch("https://onesignal.com/api/v1/notifications", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Basic ofyb7firtubtnb5jiu2aosyez",
-      },
-      body: JSON.stringify(message),
-    });
+    const response = await fetch(
+      "https://onesignal.com/api/v1/notifications",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Basic ofyb7firtubtnb5jiu2aosyez", // CORRECT KEY
+        },
+        body: JSON.stringify(message),
+      }
+    );
 
     const result = await response.json();
     return res.status(200).json(result);
